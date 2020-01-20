f1222ae7b367f6de5f43d4acc550829f5d9de229
Jason Mitchell <mitchej@gmail.com>
2020-01-19 22:01:34 -0800

    Remove analytics

--- src/main/java/ftb/lib/mod/FTBLibEventHandler.java
+++ src/main/java/ftb/lib/mod/FTBLibEventHandler.java
@@ -1,35 +1,26 @@
 package ftb.lib.mod;
 
-import com.tamashenning.forgeanalytics.client.ForgeAnalyticsConstants;
-import com.tamashenning.forgeanalytics.events.AnalyticsEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.*;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.relauncher.Side;
-import ftb.lib.*;
+import ftb.lib.FTBLib;
+import ftb.lib.FTBWorld;
 import ftb.lib.api.ServerTickCallback;
-import ftb.lib.mod.net.*;
+import ftb.lib.mod.net.MessageReload;
+import ftb.lib.mod.net.MessageSendWorldID;
 import latmod.lib.util.Phase;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.WorldEvent;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FTBLibEventHandler
 {
 	public static final List<ServerTickCallback> callbacks = new ArrayList<>();
 	public static final List<ServerTickCallback> pendingCallbacks = new ArrayList<>();
-	
-	@cpw.mods.fml.common.Optional.Method(modid = "forgeanalytics")
-	@SubscribeEvent
-	public void onAnalytics(AnalyticsEvent event)
-	{
-		FTBWorld w = FTBWorld.get(event.side);
-		if(w != null)
-		{
-			ForgeAnalyticsConstants.CustomProperties.put("FTB_PackMode", w.getMode().getID());
-		}
-	}
 	
 	@SubscribeEvent
 	public void onWorldLoaded(WorldEvent.Load e)
